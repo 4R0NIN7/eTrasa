b603076007dcf462c227c33e0b5c8f54c16e60c6
4R0NIN7 <38487438+4R0NIN7@users.noreply.github.com>
2019-11-27 13:40:23 +0100

    Sprawdzenie aplikacji
    1. Poprawa layoutów.
    2. Imie zmienia ale nie odświeża, czyli trzeba dać odświeżanie imienia.
    3. Dodanie dalszych opcji
    4. Dodanie wylogowania po zmianie nazwy
    5. Dodanie wylogowania po zmianie hasla i emaila.
    6. Dodanie resetu hasła

--- app/src/main/java/com/r0nin/etrasa/ManageProfile.java
+++ app/src/main/java/com/r0nin/etrasa/ManageProfile.java
@@ -3,7 +3,9 @@ package com.r0nin.etrasa;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -31,7 +33,7 @@ Termin tego
 
 public class ManageProfile extends AppCompatActivity {
 
-    protected Button buttonImage, buttonEmail, buttonPassword, buttonSave;
+    protected Button buttonImage, buttonEmail, buttonPassword, buttonBack, buttonName;
     protected TextView textViewNameDisplay, textViewEmailDisplay;
     protected ImageView imageViewImage;
     @Override
@@ -42,7 +44,8 @@ public class ManageProfile extends AppCompatActivity {
         buttonEmail = findViewById(R.id.buttonEmail);
         buttonImage = findViewById(R.id.buttonImage);
         buttonPassword = findViewById(R.id.buttonPassword);
-        buttonSave = findViewById(R.id.buttonChangeName);
+        buttonName = findViewById(R.id.buttonChangeName);
+        buttonBack = findViewById(R.id.buttonBack);
         textViewEmailDisplay = findViewById(R.id.textViewEmailDisplay);
         textViewNameDisplay = findViewById(R.id.textViewNameDisplay);
         imageViewImage = findViewById(R.id.imageViewImage);
@@ -50,18 +53,30 @@ public class ManageProfile extends AppCompatActivity {
         final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         textViewNameDisplay.setText(firebaseUser.getDisplayName());
         textViewEmailDisplay.setText(firebaseUser.getEmail());
+
+        buttonEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ManageProfile.this,ChangeEmail.class);
+                startActivity(intent);
+            }
+        });
+        buttonPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ManageProfile.this,ChangePassword.class);
+                startActivity(intent);
+            }
+        });
+        buttonName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ManageProfile.this,ChangeName.class);
+                startActivity(intent);
+            }
+        });
     }
 
-
-    private void updateName(FirebaseUser firebaseUser){
-        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().setDisplayName(textViewNameDisplay.getText().toString()).build();
-        firebaseUser.updateProfile(profileUpdates);
-    }
-
-
-    private void updateImage(){
-
-    }
 
 
 }
